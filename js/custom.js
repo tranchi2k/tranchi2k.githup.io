acbbba1b353f6adc9d6f806ba7acb68c32b7d54f
TranChi9 <97968751+TranChi9@users.noreply.github.com>
2022-05-13 22:55:25 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+new WOW().init();
+var typed = new Typed('.typing', {
+    strings: ["Web Designer.", "Code Developer.","Apps Designer.", "Beginner PHP"],
+    typeSpeed:50,
+    backSpeed:50,
+    loop:true,
+    });// JavaScript Document
